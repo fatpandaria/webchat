9f7468d9d2147c91842225be741eb2f3c8716278
teng <ytenghust@gmail.com>
2019-06-30 22:20:38 +0800

    remove protocol from host

--- src/containers/WebChat.jsx
+++ src/containers/WebChat.jsx
@@ -100,7 +100,7 @@ class WebChat extends React.Component {
         }
         this.state.uniqueId = uniqueId;
         console.log(this.state.uniqueId)
-        let host = env === 'development' ? 'localhost': 'https://webchat.yutengrock.com'; //FIXME:use config to make it
+        let host = env === 'development' ? 'localhost': 'webchat.yutengrock.com'; //FIXME:use config to make it
         let peer = new Peer(uniqueId,{host:host,port:9000,path:'/myapp'}); // supposed to pass id in,omit it will get a random one from the server
         this.setState({peer});
         peer.on('connection', (conn) => {
